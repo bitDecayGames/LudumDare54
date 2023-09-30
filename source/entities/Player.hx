1b1230e2ee438085d878b77cfc8f1dbe5b3b3500
Logan <monday.hopscotch@gmail.com>
2023-09-30 16:03:31 -0600

    better player movement. slight snap to 45 degree angles

--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -20,6 +20,11 @@ class Player extends IsoSprite {
 	var turnSpeed:Float = 130;
 	var playerNum = 0;
 
+	var rawAngle:Float = 0;
+	var calculatedAngle:Float = 0;
+
+	var snapToleranceDegrees = 10;
+
 	public function new() {
 		gridWidth = 1;
 		gridLength = 1;
@@ -41,24 +46,39 @@ class Player extends IsoSprite {
 		super.update(delta);
 
 		if (SimpleController.pressed(LEFT)) {
-			angle -= turnSpeed * delta;
+			rawAngle -= turnSpeed * delta;
 		}
 
 		if (SimpleController.pressed(RIGHT)) {
-			angle += turnSpeed * delta;
+			rawAngle += turnSpeed * delta;
+		}
+
+		if (rawAngle < 0) rawAngle += 360;
+		if (rawAngle >= 360) rawAngle -= 360;
+
+		if (rawAngle % 45 <= snapToleranceDegrees) {
+			calculatedAngle = rawAngle - (rawAngle % 45);
+		} else if (rawAngle % 45 >= (45 - snapToleranceDegrees)) {
+			calculatedAngle = rawAngle + (45 - (rawAngle % 45));
+		} else {
+			calculatedAngle = rawAngle;
 		}
 
 		// aka 16 segments
-		var segmentSize = 360 / 16;
+		var segmentSize = 360.0 / 16;
 		var halfSegment = segmentSize / 2;
 
-		var angleDeg = angle;
+		var angleDeg = rawAngle;
 		var intAngle = FlxMath.wrap(cast angleDeg + halfSegment, 0, 359);
-		var segments = Std.int(intAngle / segmentSize);
-		sprite.animation.frameIndex = segments;
+		var spinFrame = Std.int(intAngle / segmentSize);
+		sprite.animation.frameIndex = spinFrame;
 
 		var movement = FlxPoint.weak(speed, 0);
-		movement.rotateByDegrees(angle);
+		movement.rotateByDegrees(calculatedAngle);
 		velocity.copyFrom(movement);
+
+		FlxG.watch.addQuick('pAngRaw:', rawAngle);
+		FlxG.watch.addQuick('pAngCalc:', calculatedAngle);
+		FlxG.watch.addQuick('pAngFrame:', spinFrame);
 	}
 }
