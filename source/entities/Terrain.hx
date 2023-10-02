e8afc0ced32506ae97b7a37aa2b6d073c2c5d5bf
Logan <monday.hopscotch@gmail.com>
2023-10-02 15:28:16 -0600

    terrain attempt

--- source/entities/Terrain.hx
+++ source/entities/Terrain.hx
@@ -1,10 +1,11 @@
 package entities;
 
+import iso.IsoEchoSprite;
 import flixel.FlxSprite;
 
 import iso.IsoSprite;
 
-class Terrain extends IsoSprite {
+class Terrain extends IsoEchoSprite {
     public function new(X:Float, Y:Float, frameIndex:Int) {
         gridLength = 1;
         gridWidth = 1;
@@ -12,11 +13,16 @@ class Terrain extends IsoSprite {
         gridHeight = 0;
         super(X, Y);
         alpha = .5;
+    }
 
+    override function configSprite() {
         sprite = new FlxSprite();
         sprite.loadGraphic(AssetPaths.tiles__png, true, 32, 16);
         sprite.offset.set(sprite.width/2, sprite.height);
-
         sprite.animation.frameIndex = frameIndex;
+    }
+
+    override function setBounds() {
+        
     }
 }
