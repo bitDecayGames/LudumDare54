2a32269987127fe0f7b048fa4ed690ac5934e2a1
mwingfield <mwingfie@gmail.com>
2023-10-02 16:19:41 -0600

    update levels to look a bit better

--- source/states/debug/MikeState.hx
+++ source/states/debug/MikeState.hx
@@ -5,8 +5,10 @@ import states.PlayState;
 
 class MikeState extends PlayState {
 	override public function create() {
-		initialLevelName = "Jake_01";
+		initialLevelName = "Mike_01";
 		super.create();
+		#if FLX_DEBUG
 		FlxG.debugger.visible = true;
+		#end
 	}
 }
